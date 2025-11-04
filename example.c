419897ebc331076f63b635fea457ccc49cd3b399
GeorgiosMasouros <ge.masouros@gmail.com>
2025-11-04 14:36:02 +0200

    Update example.c

--- example.c
+++ example.c
@@ -2,8 +2,7 @@
 
 // Παράδειγμα προγράμματος που αποτελείται από ένα μόνο αρχείο .c.
 //
-// - Το τρέχουμε με Ctrl-Shift-B (ή Terminal/ Run task / Single file: compile and run)
-// - Κάνουμε debug με F5
+// -
 
 int main() {
     printf("This is a simple C program.\n");
